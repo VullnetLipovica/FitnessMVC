2ef7884258a7bda75eb0ac47a83b616bdf3d5bc3
VullnetLipovica <vl57425@ubt-uni.net>
2023-12-22 16:53:54 +0100

    made the update image nullable

--- FitnessMVC/ViewModels/EditExerciseViewModel.cs
+++ FitnessMVC/ViewModels/EditExerciseViewModel.cs
@@ -7,20 +7,19 @@ namespace FitnessMVC.ViewModels
     {
         public int ExerciseId { get; set; }
 
-        [Required(ErrorMessage = "Exercise Name is required.")]
+        
         public string exName { get; set; }
 
-        [Required(ErrorMessage = "Description is required.")]
+        
         public string exDescription { get; set; }
 
-        [Required(ErrorMessage = "Body Part is required.")]
+       
         public BodyParts BodyParts { get; set; }
 
-        [Required(ErrorMessage = "Difficulty is required.")]
         public Difficulty Difficulty { get; set; }
 
         // Property for the new image
-        public IFormFile NewImage { get; set; }
+        public IFormFile? NewImage { get; set; }
 
 
 
